f9f8f062d942594c34b30938f6670ac71da27573
anadoly <ahmedelanadoly@gmail.com>
2018-01-16 14:02:23 +0200

    Fix img

--- src/components/Admin/AdminItem/index.js
+++ src/components/Admin/AdminItem/index.js
@@ -13,7 +13,7 @@ export default class Item extends Component {
         return (
         <div className= 'oneItem clearfix'>
         <Card>
-        <CardImg className= 'cardImage' top width="50px" src={item.image_url === null? "../../../../images/makmak.png": item.image_url} alt={`img for ${item.name}`} />
+        <CardImg className= 'cardImage' top width="50px" src={item.image_url === null? "https://media-exp2.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAf-AAAAJDAyZjY1Njc3LWQyMzItNDk3ZC1hOTk0LTJhNGY2MGUxMmQzZg.png": item.image_url} alt={`img for ${item.name}`} />
        <CardBody>
           <CardText>{item.name}</CardText>
           <CardText>EGP {item.price}</CardText>
